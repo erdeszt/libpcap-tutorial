ec9cfdcc86a72f4d52944aae05539cf2efbc4398
erdeszt <erdeszt@gmail.com>
2013-03-24 22:15:41 +0100

    Formatting capture.c to 2 spaces

--- capture.c
+++ capture.c
@@ -16,117 +16,122 @@
 #include <time.h>
 
 int main(int argc, char **argv) {
-    int length;
-    char error_buffer[PCAP_ERRBUF_SIZE];
-    char* device;
-    u_char* packet;    
-    u_char* host;
-    pcap_t* handler;    
-    struct pcap_pkthdr packet_header;
-    struct ether_header *ethernet_header;
+  int length;
+  char error_buffer[PCAP_ERRBUF_SIZE];
+  char* device;
+  u_char* packet;    
+  u_char* host;
+  pcap_t* handler;    
+  struct pcap_pkthdr packet_header;
+  struct ether_header *ethernet_header;
     
     /* grab a device to peak into */
+  if (argc > 1) {
+    device = argv[1];
+  }
+  else {
     device = pcap_lookupdev(error_buffer);
 
-    if(device == NULL) {
-        fprintf(stderr, "%s\n", error_buffer); 
-        exit(1);
+    if (device == NULL) {
+      fprintf(stderr, "pcap_loookupdev(): %s\n", error_buffer);
+      exit(1);
     }
+  }
 
-    printf("DEV: %s\n",device);
+  printf("DEV: %s\n",device);
 
-    /* Open the device for sniffing. 
+  /* Open the device for sniffing. 
 
-    pcap_t *pcap_open_live(char *device, int snaplen, int prmisc, int to_ms,
-    char *ebuf);
-    snaplen - maximum size of packets to capture in bytes
-    promisc - set card in promiscuous mode?
-    to_ms   - time to wait for packets in miliseconds before read times out
-    errbuf  - if something happens, place error string here
+  pcap_t *pcap_open_live(char *device, int snaplen, int prmisc, int to_ms,
+  char *ebuf);
+  snaplen - maximum size of packets to capture in bytes
+  promisc - set card in promiscuous mode?
+  to_ms   - time to wait for packets in miliseconds before read times out
+  errbuf  - if something happens, place error string here
 
-    Note if you change "promisc" param to anything other than zero, you will
-    get all packets your device sees, whether they are intended for you or
-    not!! Be sure you know the rules of the network you are running on
-    before you set your card in promiscuous mode!!  */
+  Note if you change "promisc" param to anything other than zero, you will
+  get all packets your device sees, whether they are intended for you or
+  not!! Be sure you know the rules of the network you are running on
+  before you set your card in promiscuous mode!!  */
 
-    handler = pcap_open_live(device, BUFSIZ, 0, -1, error_buffer);
+  handler = pcap_open_live(device, BUFSIZ, 0, -1, error_buffer);
 
-    if(handler == NULL) {
-        printf("pcap_open_live(): %s\n", error_buffer);
-        exit(1);
-    }
+  if(handler == NULL) {
+    printf("pcap_open_live(): %s\n", error_buffer);
+    exit(1);
+  }
 
-    /* Grab a packet from handler
-     *
-     * u_char *pcap_next(pcap_t *p,struct pcap_pkthdr *h)
-     * so just pass in the descriptor we got from
-     * our call to pcap_open_live and an allocated
-     * struct pcap_pkthdr                               
+  /* Grab a packet from handler
+   *
+   * u_char *pcap_next(pcap_t *p,struct pcap_pkthdr *h)
+   * so just pass in the descriptor we got from
+   * our call to pcap_open_live and an allocated
+   * struct pcap_pkthdr                               
+   */
+
+  packet = pcap_next(handler, &packet_header);
+
+  if(packet == NULL) {
+    /* Don't worry if you don't grab a packet at this point 
+     * Try another example where we listen to the traffic and not just trying to 
+     * grab a packet.
      */
+    printf("Didn't grab packet\n"); 
+    exit(1);
+  }
 
-    packet = pcap_next(handler, &packet_header);
+  /* struct pcap_pkthdr {
+   *   struct timeval ts;   time stamp
+   *   bpf_u_int32 caplen;  length of portion present
+   *   bpf_u_int32;         lebgth this packet (off wire)
+   * }
+   */
 
-    if(packet == NULL) {
-        /* Don't worry if you don't grab a packet at this point 
-         * Try another example where we listen to the traffic and not just trying to 
-         * grab a packet.
-         */
-        printf("Didn't grab packet\n"); 
-        exit(1);
-    }
+  printf("Grabbed packet of length . %d\n", packet_header.len);
+  printf("Recieved at .............. %s\n", ctime((const time_t*)&packet_header.ts.tv_sec));
+  printf("Ethernet address length is %d\n", ETHER_HDR_LEN);
 
-    /* struct pcap_pkthdr {
-     *   struct timeval ts;   time stamp
-     *   bpf_u_int32 caplen;  length of portion present
-     *   bpf_u_int32;         lebgth this packet (off wire)
-     * }
-     */
+  /* Lets start with the ether header */
+  ethernet_header = (struct ether_header*)packet;
 
-    printf("Grabbed packet of length . %d\n", packet_header.len);
-    printf("Recieved at .............. %s\n", ctime((const time_t*)&packet_header.ts.tv_sec));
-    printf("Ethernet address length is %d\n", ETHER_HDR_LEN);
+  /* Do a couple of checks to see what packet type we have */
+  if (ntohs(ethernet_header->ether_type) == ETHERTYPE_IP) {
+    printf("Ethernet type hex: %x dec: %d is an IP packet\n",
+            ntohs(ethernet_header->ether_type),
+            ntohs(ethernet_header->ether_type));
+  }
+  else if (ntohs(ethernet_header->ether_type) == ETHERTYPE_ARP) {
+    printf("Ethernet type hex: %x dec: %d is an ARP packet\n",
+            ntohs(ethernet_header->ether_type),
+            ntohs(ethernet_header->ether_type));
+  }
+  else {
+    printf("Ethernet type %x not IP", ntohs(ethernet_header->ether_type));
+    exit(1);
+  }
 
-    /* Lets start with the ether header */
-    ethernet_header = (struct ether_header*)packet;
+  /* THANK YOU RICHARD STEVENS!!! RIP */
+  host = ethernet_header->ether_dhost;
+  length = ETHER_ADDR_LEN;
 
-    /* Do a couple of checks to see what packet type we have */
-    if (ntohs(ethernet_header->ether_type) == ETHERTYPE_IP) {
-        printf("Ethernet type hex: %x dec: %d is an IP packet\n",
-                ntohs(ethernet_header->ether_type),
-                ntohs(ethernet_header->ether_type));
-    }
-    else if (ntohs(ethernet_header->ether_type) == ETHERTYPE_ARP) {
-        printf("Ethernet type hex: %x dec: %d is an ARP packet\n",
-                ntohs(ethernet_header->ether_type),
-                ntohs(ethernet_header->ether_type));
-    }
-    else {
-        printf("Ethernet type %x not IP", ntohs(ethernet_header->ether_type));
-        exit(1);
-    }
+  printf(" Destination address: ");
 
-    /* THANK YOU RICHARD STEVENS!!! RIP */
-    host = ethernet_header->ether_dhost;
-    length = ETHER_ADDR_LEN;
+  do {
+    printf("%s%x",(length == ETHER_ADDR_LEN) ? " " : ":", *host++);
+  } while (--length > 0);
 
-    printf(" Destination address: ");
+  printf("\n");
 
-    do {
-        printf("%s%x",(length == ETHER_ADDR_LEN) ? " " : ":", *host++);
-    } while (--length > 0);
+  host = ethernet_header->ether_shost;
+  length = ETHER_ADDR_LEN;
+  
+  printf(" Source address: ");
 
-    printf("\n");
+  do {
+    printf("%s%x",(length == ETHER_ADDR_LEN) ? " " : ":", *host++);
+  } while(--length > 0);
 
-    host = ethernet_header->ether_shost;
-    length = ETHER_ADDR_LEN;
-    
-    printf(" Source address: ");
+  printf("\n");
 
-    do {
-        printf("%s%x",(length == ETHER_ADDR_LEN) ? " " : ":", *host++);
-    } while(--length > 0);
-
-    printf("\n");
-
-    return 0;
+  return 0;
 }
